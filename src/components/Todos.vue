0d3af414bf72f63364c594fd89a57ac85d299070
Andres <anerdspual@outlook.com>
2019-07-15 22:30:44 +0200

    Add project

---
+++ src/components/Todos.vue
@@ -0,0 +1,20 @@
+<template>
+
+    <div>
+        <div :key="todo.id" v-for="todo in todos">
+            <h1>{{todo.title}}</h1>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Todos",
+        props: ["todos"]
+    }
+</script>
+
+<style scoped>
+
+</style>
