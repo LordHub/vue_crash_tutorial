996659e68903dd0e4db680478c8db5c310280d71
Andres <anerdspual@outlook.com>
2019-08-10 11:54:53 +0200

    Finish the course

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,19 @@
 <template>
   <div id="app">
-    <Header />
-    <add-todo v-on:addTodo="addTodo"></add-todo>
-    <todos :todos="todos" v-on:del-todo=deleteTodo></todos>
+    <Header></Header>
+    <router-view/>
   </div>
 </template>
-
 <script>
-
-import Todos from './components/Todos'
-import AddTodo from './components/AddTodo'
-import Header from './components/layouts/Header'
-import axios from 'axios';
-
-export default {
-  name: 'app',
-  components: {
-    Todos,
-    Header,
-    AddTodo
-  },
-  data() {
-    return {
-      todos: []
+  import Header from './components/layouts/Header.vue'
+  export default {
+    name: 'app',
+    components: {
+      Header
     }
-  },
-  methods: {
-    deleteTodo(id){
 
-      axios.post(`https://jsonplaceholder.typicode.com/todos/${id}`,{
-        title,
-        completed
-      })
-      .then((response) => {
-        this.todos = this.todos.filter(todo => todo.id !== id)
-      })
-      .catch(err => console.log(err))
-    },
-    addTodo(todo) {
-      const { title, completed } = todo;
-      axios.post('https://jsonplaceholder.typicode.com/todos',{
-        title,
-        completed
-      })
-      .then((response) => {
-        this.todos = [...this.todos, todo]
-      })
-      .catch(err => console.log(err))
-
-    }
-  },
-  created() {
-    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
-    .then((response) => {
-      this.todos = response.data
-    })
-    .catch(err => console.log(err))
   }
-}
 </script>
-
 <style>
   * {
     box-sizing: border-box;
---
+++ src/views/Home.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+    <add-todo v-on:addTodo="addTodo"></add-todo>
+    <todos :todos="todos" v-on:del-todo=deleteTodo></todos>
+  </div>
+</template>
+
+<script>
+
+  import Todos from '../components/Todos'
+  import AddTodo from '../components/AddTodo'
+  import axios from 'axios';
+
+  export default {
+    name: 'Home',
+    components: {
+      Todos,
+      AddTodo
+    },
+    data() {
+      return {
+        todos: []
+      }
+    },
+    methods: {
+      deleteTodo(id){
+
+        axios.post(`https://jsonplaceholder.typicode.com/todos/${id}`,{
+          title,
+          completed
+        })
+                .then((response) => {
+                  this.todos = this.todos.filter(todo => todo.id !== id)
+                })
+                .catch(err => console.log(err))
+      },
+      addTodo(todo) {
+        const { title, completed } = todo;
+        axios.post('https://jsonplaceholder.typicode.com/todos',{
+          title,
+          completed
+        })
+                .then((response) => {
+                  this.todos = [...this.todos, todo]
+                })
+                .catch(err => console.log(err))
+
+      }
+    },
+    created() {
+      axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+              .then((response) => {
+                this.todos = response.data
+              })
+              .catch(err => console.log(err))
+    }
+  }
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+</style>
